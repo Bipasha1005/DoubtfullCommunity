55144570b43f84342b2aa4742aebbebcd5c48dbb
Bipasha_Acharjee <118049787+Bipasha1005@users.noreply.github.com>
2024-02-18 17:59:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+    $('.team-cards').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                }
+            }
+        ]
+    });
+});
